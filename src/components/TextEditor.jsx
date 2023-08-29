1077cb9cba2cea97d04a967094902fd1794ce14b
777Hemant <hemantmishra12012000@gmail.com>
2023-08-29 21:43:11 +0530

    first commit

---
+++ src/components/TextEditor.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
+
+
+const TextEditor = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+
+  return (
+    <div className="background-gradient">
+      <div>
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={onEditorStateChange}
+        />
+     
+      </div>
+    
+    </div>
+  );
+};
+
+export default TextEditor;
